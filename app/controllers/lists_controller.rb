bba4e2b88c9e6207c6e156bb90e6a0b7fa3f62b4
AyakaTakahashi <132245813+suco31a42@users.noreply.github.com>
2023-05-04 07:49:50 +0000

    [finish]3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -18,7 +18,14 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
-  def edit
+  def edit #編集フォーム
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
   end
   
   private
