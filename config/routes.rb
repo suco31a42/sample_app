11f85e3160a8ce503f5067baef331a7bb469591d
AyakaTakahashi <132245813+suco31a42@users.noreply.github.com>
2023-05-04 11:33:39 +0000

    [finish]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   get 'top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  # .../lists/1 や .../lists/3 に該当する
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'listd/:id' => 'lists#update', as: 'update_list'
-  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+  resources :lists
+  
 end
