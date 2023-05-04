6cff7a2c5223c3c6e3024b62809f1ad2cddd0772
AyakaTakahashi <132245813+suco31a42@users.noreply.github.com>
2023-05-04 01:45:29 +0000

    [add]3-2作業再開

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,15 @@
 class ListsController < ApplicationController
+  
   def new
+    @list = List.new
   end
-
+  
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+  end
+    
   def index
   end
 
@@ -9,5 +17,11 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+  #ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
